02bc54c8f45af0a51541f6c73ef0c17e2ddeb9af
Matheus Tenorio <matheus.santos1899@gmail.com>
2020-07-22 20:35:31 -0300

    Titlescreen layout has been updated. Add landscape layout. Style changed.

--- app/src/main/java/uk/co/philpotter/masteremu/TitlescreenActivity.java
+++ app/src/main/java/uk/co/philpotter/masteremu/TitlescreenActivity.java
@@ -24,16 +24,15 @@ import android.util.Log;
 public class TitlescreenActivity extends Activity {
 
     // define instance variables
-    private ControllerSelection selectionObj;
-    private long timeSinceLastAnaloguePress = 0;
+    //private ControllerSelection selectionObj;
+    //private long timeSinceLastAnaloguePress = 0;
 
     /**
      * This loads settings when the app first starts.
      */
-    @Override
-    protected void onStart() {
+    @Override protected void onStart() {
         super.onStart();
-        File f = new File(getFilesDir() + "/default_file");
+        /*File f = new File(getFilesDir() + "/default_file");
         try {
             f.createNewFile();
         }
@@ -49,14 +48,13 @@ public class TitlescreenActivity extends Activity {
             }
         } else {
             setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR);
-        }
+        }*/
     }
 
     /**
      * This kills the app for us when back is pressed.
      */
-    @Override
-    public void onBackPressed() {
+    @Override public void onBackPressed() {
         finish();
     }
 
@@ -64,8 +62,7 @@ public class TitlescreenActivity extends Activity {
      * This method creates the titlescreen.
      */
     @SuppressWarnings("deprecation")
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         try {
@@ -77,7 +74,7 @@ public class TitlescreenActivity extends Activity {
             finish();
         }
         setContentView(R.layout.titlescreen_activity);
-
+        /*
         // Add listeners
         ButtonColourListener bcl = new ButtonColourListener();
         ControllerTextView title_load_button = (ControllerTextView)findViewById(R.id.title_load_button);
@@ -96,39 +93,8 @@ public class TitlescreenActivity extends Activity {
         title_exit_button.setOnTouchListener(bcl);
 
         // Change visiblity
-        findViewById(R.id.titlescreen_wait).setVisibility(View.GONE);
+        findViewById(R.id.txtView_Wait).setVisibility(View.GONE);
         findViewById(R.id.titlescreen_go).setVisibility(View.VISIBLE);
-
-        // Set all buttons to same width
-        int longest = 0;
-        title_load_button.measure(View.MeasureSpec.UNSPECIFIED, View.MeasureSpec.UNSPECIFIED);
-        title_options_button.measure(View.MeasureSpec.UNSPECIFIED, View.MeasureSpec.UNSPECIFIED);
-        title_help_button.measure(View.MeasureSpec.UNSPECIFIED, View.MeasureSpec.UNSPECIFIED);
-        title_credits_button.measure(View.MeasureSpec.UNSPECIFIED, View.MeasureSpec.UNSPECIFIED);
-        title_extras_button.measure(View.MeasureSpec.UNSPECIFIED, View.MeasureSpec.UNSPECIFIED);
-        title_manage_states_button.measure(View.MeasureSpec.UNSPECIFIED, View.MeasureSpec.UNSPECIFIED);
-        title_exit_button.measure(View.MeasureSpec.UNSPECIFIED, View.MeasureSpec.UNSPECIFIED);
-        if (title_load_button.getMeasuredWidth() > longest)
-            longest = title_load_button.getMeasuredWidth();
-        if (title_options_button.getMeasuredWidth() > longest)
-            longest = title_options_button.getMeasuredWidth();
-        if (title_help_button.getMeasuredWidth() > longest)
-            longest = title_help_button.getMeasuredWidth();
-        if (title_credits_button.getMeasuredWidth() > longest)
-            longest = title_credits_button.getMeasuredWidth();
-        if (title_extras_button.getMeasuredWidth() > longest)
-            longest = title_extras_button.getMeasuredWidth();
-        if (title_manage_states_button.getMeasuredWidth() > longest)
-            longest = title_manage_states_button.getMeasuredWidth();
-        if (title_exit_button.getMeasuredWidth() > longest)
-            longest = title_exit_button.getMeasuredWidth();
-        title_load_button.setLayoutParams(new LayoutParams(longest, title_load_button.getMeasuredHeight()));
-        title_options_button.setLayoutParams(new LayoutParams(longest, title_options_button.getMeasuredHeight()));
-        title_help_button.setLayoutParams(new LayoutParams(longest, title_help_button.getMeasuredHeight()));
-        title_credits_button.setLayoutParams(new LayoutParams(longest, title_credits_button.getMeasuredHeight()));
-        title_extras_button.setLayoutParams(new LayoutParams(longest, title_extras_button.getMeasuredHeight()));
-        title_manage_states_button.setLayoutParams(new LayoutParams(longest, title_manage_states_button.getMeasuredHeight()));
-        title_exit_button.setLayoutParams(new LayoutParams(longest, title_exit_button.getMeasuredHeight()));
 
         // Set drawables on buttons and create their intents
         Drawable light = null;
@@ -152,33 +118,41 @@ public class TitlescreenActivity extends Activity {
         title_load_button.setInactiveDrawable(light);
         title_load_button.setHighlightedTextColour(darkText);
         title_load_button.setUnhighlightedTextColour(lightText);
+        //
         title_options_button.setActiveDrawable(dark);
         title_options_button.setInactiveDrawable(light);
         title_options_button.setHighlightedTextColour(darkText);
         title_options_button.setUnhighlightedTextColour(lightText);
+        //
         title_help_button.setActiveDrawable(dark);
         title_help_button.setInactiveDrawable(light);
         title_help_button.setHighlightedTextColour(darkText);
         title_help_button.setUnhighlightedTextColour(lightText);
+        //
         title_credits_button.setActiveDrawable(dark);
         title_credits_button.setInactiveDrawable(light);
         title_credits_button.setHighlightedTextColour(darkText);
         title_credits_button.setUnhighlightedTextColour(lightText);
+        //
         title_extras_button.setActiveDrawable(dark);
         title_extras_button.setInactiveDrawable(light);
         title_extras_button.setHighlightedTextColour(darkText);
         title_extras_button.setUnhighlightedTextColour(lightText);
+        //
         title_manage_states_button.setActiveDrawable(dark);
         title_manage_states_button.setInactiveDrawable(light);
         title_manage_states_button.setHighlightedTextColour(darkText);
         title_manage_states_button.setUnhighlightedTextColour(lightText);
+        //
         title_exit_button.setActiveDrawable(dark);
         title_exit_button.setInactiveDrawable(light);
         title_exit_button.setHighlightedTextColour(darkText);
         title_exit_button.setUnhighlightedTextColour(lightText);
+        //
         Intent titleIntent = new Intent(TitlescreenActivity.this, FileBrowser.class);
         titleIntent.putExtra("actionType", "load_rom");
         title_load_button.setIntent(titleIntent);
+        //
         title_options_button.setIntent(new Intent(TitlescreenActivity.this, OptionsActivity.class));
         title_help_button.setIntent(new Intent(TitlescreenActivity.this, HelpActivity.class));
         title_credits_button.setIntent(new Intent(TitlescreenActivity.this, CreditsActivity.class));
@@ -201,11 +175,13 @@ public class TitlescreenActivity extends Activity {
         // Set new MasterEmuMotionListener
         View titlescreen_root = findViewById(R.id.titlescreen_root);
         titlescreen_root.setOnGenericMotionListener(new MasterEmuMotionListener());
+        */
     }
 
     /**
      * This method helps us to detect gamepad events and do the right thing with them.
      */
+    /*
     @Override
     public boolean dispatchKeyEvent(KeyEvent event) {
         int action, keycode;
@@ -236,11 +212,12 @@ public class TitlescreenActivity extends Activity {
             return returnVal;
 
         return super.dispatchKeyEvent(event);
-    }
+    }*/
 
     /**
      * This method deals with input from the analogue stick and some d-pads.
      */
+    /*
     public boolean motionEvent(MotionEvent event) {
         boolean returnVal = true;
 
@@ -282,26 +259,26 @@ public class TitlescreenActivity extends Activity {
 
         return returnVal;
     }
-
+    */
     /**
      * This inner class allows us to support joysticks with a listener.
      */
-    protected class MasterEmuMotionListener implements View.OnGenericMotionListener {
+    /*protected class MasterEmuMotionListener implements View.OnGenericMotionListener {
         @Override
         public boolean onGenericMotion(View v, MotionEvent event) {
             return TitlescreenActivity.this.motionEvent(event);
         }
-    }
+    }*/
 
     /**
      * This class allows us to animate a button.
      */
-    protected class ButtonColourListener implements View.OnTouchListener {
+    /*protected class ButtonColourListener implements View.OnTouchListener {*/
 
         /**
          * This is the actual implementation code.
          */
-        @Override
+        /*@Override
         public boolean onTouch(View v, MotionEvent event) {
             ControllerTextView cv = (ControllerTextView)v;
             if (event.getAction() == MotionEvent.ACTION_DOWN) {
@@ -310,9 +287,8 @@ public class TitlescreenActivity extends Activity {
                 cv.unHighlight();
                 cv.activate();
             }
-
             return true;
         }
-    }
+    }*/
 
 }
